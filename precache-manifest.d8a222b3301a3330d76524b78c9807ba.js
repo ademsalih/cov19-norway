01bb6cfade1162c837465a1d7cc99552d005b582
Adem Salih <ademsal.97@icloud.com>
2020-02-29 20:59:23 +0100

    Updates

---
+++ precache-manifest.d8a222b3301a3330d76524b78c9807ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb1a164e4194a34ac5272558705f9ef",
+    "url": "/cov19-norway/index.html"
+  },
+  {
+    "revision": "3d3363a752014048be13",
+    "url": "/cov19-norway/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1ff61354ddf5411ff837",
+    "url": "/cov19-norway/static/js/2.7ae2ed84.chunk.js"
+  },
+  {
+    "revision": "d2cf8d37946cd02abf2fe73b5ccb09c4",
+    "url": "/cov19-norway/static/js/2.7ae2ed84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d3363a752014048be13",
+    "url": "/cov19-norway/static/js/main.b80a298c.chunk.js"
+  },
+  {
+    "revision": "931cbb22c4ee141265a0",
+    "url": "/cov19-norway/static/js/runtime-main.7cefeabc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cov19-norway/static/media/logo.5d5d9eef.svg"
+  }
+]);
